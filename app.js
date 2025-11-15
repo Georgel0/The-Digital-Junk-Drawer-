b3d19c799184ccaf8e79835fe00061fe57312c02
Georgel0 <georgelgarabajiu07@gmail.com>
2025-11-15 20:55:31 +0200

    update

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+// Eye-tracking logic
+document.querySelector('body').addEventListener('mousemove', (e) => {
+ const eye = document.querySelector('.eye-socket');
+ const iris = document.querySelector('.iris');
+ const rect = eye.getBoundingClientRect();
+ 
+ const eyeCenterX = rect.left + rect.width / 2;
+ const eyeCenterY = rect.top + rect.height / 2;
+ 
+ let deltaX = e.clientX - eyeCenterX;
+ let deltaY = e.clientY - eyeCenterY;
+ 
+ const angle = Math.atan2(deltaY, deltaX);
+ 
+ const maxDistance = (eye.clientWidth / 2) - (iris.clientWidth / 2);
+ 
+ const newX = maxDistance * Math.cos(angle);
+ const newY = maxDistance * Math.sin(angle);
+ 
+ iris.style.transform = `translate(calc(-50% + ${newX}px), calc(-50% + ${newY}px))`;
+});
+
+document.querySelector('.eye-socket').addEventListener('click', () => {
+ const eyeText = document.getElementById('eye-text');
+ 
+ eyeText.classList.add('visible');
+ 
+ setTimeout(() => {
+  eyeText.classList.remove('visible')
+ }, 2000)
+});
